966aad6dc2d09824f888d3db92a0a7998fe471a1
sonmodx <sonmodx@gmail.com>
2023-10-02 13:19:11 +0700

    test login

--- server/app.js
+++ server/app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const path = require('path');
-const sequelize = require('./config/database');
-const morgan = require('morgan');
-const cors = require('cors'); // Import the cors package
+const express = require("express");
+const path = require("path");
+const sequelize = require("./config/database");
+const morgan = require("morgan");
+const cors = require("cors"); // Import the cors package
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
-const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
-const authRoutes = require('./routes/authRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const cartRoutes = require('./routes/cartRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const authRoutes = require("./routes/authRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -22,19 +22,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(morgan('dev'));
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true, // Allow credentials to be sent
-}));
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // Allow credentials to be sent
+  })
+);
 
 // Swagger configuration
 const swaggerOptions = {
   definition: {
     openapi: "3.1.0",
     info: {
-      title: 'Chiqko API',
-      version: '1.0.0',
+      title: "Chiqko API",
+      version: "1.0.0",
     },
     servers: [
       {
@@ -44,45 +46,45 @@ const swaggerOptions = {
     components: {
       securitySchemes: {
         auth_token: {
-          type: 'apiKey',
-          in: 'cookie',
-          name: 'auth_token', // Specify the cookie name
+          type: "apiKey",
+          in: "cookie",
+          name: "auth_token", // Specify the cookie name
         },
       },
     },
   },
   apis: [
-    './routes/*.js', // Glob pattern to include route files in Swagger
-    './models/*.js', // Include your model files for API documentation
+    "./routes/*.js", // Glob pattern to include route files in Swagger
+    "./models/*.js", // Include your model files for API documentation
   ],
 };
 
 let swaggerSpec = swaggerJsdoc(swaggerOptions);
 
-app.use('/api/v1/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/products', productRoutes);
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/orders', orderRoutes);
-app.use('/api/v1/carts', cartRoutes);
-app.use('/api/v1/categories', categoryRoutes);
+app.use("/api/v1/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/orders", orderRoutes);
+app.use("/api/v1/carts", cartRoutes);
+app.use("/api/v1/categories", categoryRoutes);
 
 sequelize.sync({ force: true }).then(() => {
   app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log("Server is running on port 3000");
 
-    const createSampleData = require('./db/sampleData');
+    const createSampleData = require("./db/sampleData");
     createSampleData();
   });
 });
 
 // Serve Swagger documentation
-app.use('/api/v1', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api/v1", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/health", (req, res) => {
   res.status(200).send("Healthy");
 });
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not Found' });
+  res.status(404).json({ message: "Not Found" });
 });
